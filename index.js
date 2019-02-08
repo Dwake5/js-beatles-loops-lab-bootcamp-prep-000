48b13cf5507b628f73c5619d420a5031f5b8be9b
Danny Wakeling <wakeling101@hotmail.com>
2019-02-08 11:00:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   let beatles = []
-  for(let i = 0; i < thisVariable.length; i++){
+  for(let i = 0; i < beatles.length; i++){
     beatles.push(musicians[i] + " plays " + instruments[i])
   }
   return beatles
