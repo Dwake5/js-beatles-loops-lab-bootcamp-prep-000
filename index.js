d3f2342f1ddc555689988715fccf19bd0550a04f
Danny Wakeling <wakeling101@hotmail.com>
2019-02-08 10:59:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-
+  let beatles = []
+  for(let i = 0; i < thisVariable.length; i++){
+    beatles.push(musicians[i] + " plays " + instruments[i])
+  }
+  return beatles
 }
+
+function johnLennonFacts
